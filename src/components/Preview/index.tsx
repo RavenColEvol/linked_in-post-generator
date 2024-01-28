6323e8c5b0cbfcd641fd0633000bf74eb547258f
RavenColEvol <ravilamkoti@gmail.com>
2024-01-28 10:08:57 +0530

    add: highlight preview

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,42 +1,53 @@
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
 import { useFormState } from "../../hooks/stateContext";
 import { CardRenderer } from "../Renderer";
-import { DeleteOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { DeleteOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import clsx from "clsx";
 
 function Preview() {
   const [form, setFormState] = useFormState()!;
 
-  const handleDelete = (uid: string) => {
-    setFormState(form => {
-      if (form.slides.length === 1) {
-        return form;
-      }
+  const handleDelete = (event: React.MouseEvent<HTMLElement>, uid: string) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (form.slides.length === 1) {
+      return console.log("can't delete it");
+    }
+    setFormState((form) => {
       return {
         ...form,
-        slides: form.slides.filter(slide => slide.uid !== uid)
-      }
-    })
-  }
-  return (
-    <div className="preview-container" style={{fontSize: '4px'}}>
-      {form.slides.map((slide) => (
-        <CardWrapper 
-        handleSwitch={() => {
+        slides: form.slides.filter((slide) => slide.uid !== uid),
+      };
+    });
+  };
 
-        }}
-        handleDelete={(event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          handleDelete(slide.uid)
-        }} key={slide.uid}>
+  const changePreview = (index: number) => {
+    setFormState((form) => ({
+      ...form,
+      selectedIdx: index
+    }));
+  }
+
+  return (
+    <div className="preview-container" style={{ fontSize: "4px" }}>
+      {form.slides.map((slide, idx) => (
+        <CardWrapper
+          onClick={() => changePreview(idx)}
+          key={slide.uid}
+          isActive={form.selectedIdx === idx}
+        >
           <CardRenderer
             key={slide.uid}
-            style={{ width: '6rem' }}
-            slide={slide} 
-            user={form.user} 
-            theme={form.theme} 
+            style={{ width: "6rem" }}
+            slide={slide}
+            user={form.user}
+            theme={form.theme}
           />
+          <Button size="small" onClick={(e) => handleDelete(e, slide.uid)}>
+            <DeleteOutlined />
+          </Button>
         </CardWrapper>
       ))}
       <AddBlock />
@@ -44,36 +55,39 @@ function Preview() {
   );
 }
 
-function CardWrapper({ children, handleDelete, handleSwitch }: {
-  handleDelete: (e: any) => void,
-  handleSwitch: (e: any) => void,
-  children: React.ReactNode
-}) {
+const CardWrapper = (props: any) => {
+  const { isActive } = props;
   return (
-    <div onClick={handleSwitch} className='delete_wrapper'>
-      {children}
-      <Button size='small' onClick={handleDelete}><DeleteOutlined /></Button>
+    <div {...props} className={clsx('preview_card__wrapper', { 'card_wrapper--active': isActive })}>
+      {props.children}
     </div>
-  )
-}
+  );
+};
 
 function AddBlock() {
   const [, setFormState] = useFormState()!;
   const handleClick = () => {
-    setFormState(form => ({
+    setFormState((form) => ({
       ...form,
-      slides: [...form.slides, {
-        uid: v4(),
-        title: 'Your Title',
-      }]
-    }))
+      slides: [
+        ...form.slides,
+        {
+          uid: v4(),
+          title: "Your Title",
+        },
+      ],
+    }));
   };
 
   return (
-    <div onClick={handleClick} title="Add Block" className="add-slide-container">
+    <div
+      onClick={handleClick}
+      title="Add Block"
+      className="add-slide-container"
+    >
       +
     </div>
-  )
+  );
 }
 
 export default Preview;
