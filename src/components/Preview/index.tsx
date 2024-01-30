18e6995da12e311933f3dc35da5b1bc3d47b8b27
RavenColEvol <ravilamkoti@gmail.com>
2024-01-31 00:34:51 +0530

    change: dnd to react beautiful dnd

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -4,13 +4,17 @@ import { useFormState } from "../../hooks/stateContext";
 import { CardRenderer } from "../Renderer";
 import { DeleteOutlined } from "@ant-design/icons";
 import { Button, notification } from "antd";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
-import { TouchBackend } from 'react-dnd-touch-backend';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import clsx from "clsx";
 import { useRef } from "react";
+import { ISlide } from "../../App";
 
-const dndOptions = {
-  enableMouseEvents: true
+const reorder = (list: ISlide[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
 };
 
 function Preview() {
@@ -49,127 +53,95 @@ function Preview() {
     });
   };
 
+  const handleDrop = (result: any) => {
+    if (!result.destination) return;
+    setFormState(form => {
+      const newSlides = reorder(
+        form.slides, 
+        result.source.index,
+        result.destination.index
+      );
+      return {
+        ...form,
+        slides: newSlides
+      };
+    });
+  }
+
+  const handleDragStart = () => {
+    if (window.navigator.vibrate) {
+      window.navigator.vibrate(100);
+    }
+  }
+
   return (
-    <DndProvider backend={TouchBackend} options={dndOptions}>
-      <div className="preview-container">
-        {contextHolder}
-        <div className="drop_line" ref={dropLineRef}></div>
-        {form.slides.map((slide, idx) => (
-          <CardWrapper
-            onClick={() => changePreview(idx)}
-            key={slide.uid}
-            uid={slide.uid}
-            index={idx}
-            isactive={!!(form.selectedIdx === idx)}
-            droplineref={dropLineRef}
+    <DragDropContext onDragStart={handleDragStart} onDragEnd={handleDrop}>
+      <Droppable droppableId="droppable" direction="horizontal">
+        {(provided, snapshot) => (
+          <div 
+            className="preview-container"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
           >
-            <CardRenderer
-              key={slide.uid}
-              style={{ width: "6rem" }}
-              slide={slide}
-              user={form.user}
-              theme={form.theme}
-            />
-            <Button size="small" onClick={(e) => handleDelete(e, slide.uid)}>
-              <DeleteOutlined />
-            </Button>
-          </CardWrapper>
-        ))}
-        <AddBlock />
-      </div>
-    </DndProvider>
+            {contextHolder}
+            {form.slides.map((slide, idx) => (
+              <CardWrapper
+                onClick={() => changePreview(idx)}
+                key={slide.uid}
+                uid={slide.uid}
+                index={idx}
+                isactive={!!(form.selectedIdx === idx)}
+                droplineref={dropLineRef}
+              >
+                <CardRenderer
+                  key={slide.uid}
+                  style={{ width: "6rem" }}
+                  slide={slide}
+                  user={form.user}
+                  theme={form.theme}
+                />
+                <Button size="small" onClick={(e) => handleDelete(e, slide.uid)}>
+                  <DeleteOutlined />
+                </Button>
+              </CardWrapper>
+            ))}
+            <AddBlock hide={snapshot.isDraggingOver}/>
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
-const dndCardType = "preview-card";
-
-const getDirection = (cardRef: any, monitor: any) => {
-  const { left, width } = cardRef.current.getBoundingClientRect();
-  const monitorOffset = monitor.getClientOffset();
-  const mid = left + width / 2;
-  return monitorOffset!.x < mid ? "left" : "right";
-};
-
 const CardWrapper = (props: any) => {
-  const [, setFormState] = useFormState()!;
-  const { isactive, droplineref, uid, onClick } = props;
+  const { isactive, uid, onClick, index } = props;
   const cardRef = useRef<null | HTMLDivElement>(null);
-  const [{ isDragging }, drag] = useDrag(
-    () => ({
-      type: dndCardType,
-      item: {
-        uid,
-      },
-      collect: (monitor) => ({
-        isDragging: !!monitor.isDragging(),
-      }),
-      end: () => {
-        droplineref.current.style = null;
-      },
-    }),
-    [uid]
-  );
-
-  const [, drop] = useDrop(
-    () => ({
-      accept: dndCardType,
-      drop: (item, monitor) => {
-        if (item && cardRef.current) {
-          const direction = getDirection(cardRef, monitor);
-          setFormState((form) => {
-            const newState = { ...form };
-            const [from, to] = [
-              newState.slides.findIndex(slide => slide.uid === item.uid),
-              newState.slides.findIndex(slide => slide.uid === uid),
-            ];
-            const dragged = newState.slides.splice(from, 1);
-
-            newState.slides.splice(
-              direction === "left" ? to : to + 1,
-              0,
-              ...dragged
-            );
-            return newState;
-          });
-        }
-      },
-      canDrop: (item: any) => item.uid !== uid,
-      collect: (monitor) => {
-        return {
-          isOver: !!monitor.isOver(),
-        };
-      },
-      hover: (item, monitor) => {
-        if (item && cardRef.current && droplineref.current) {
-          const { left, width, top, height } =
-            cardRef.current.getBoundingClientRect();
-          const direction = getDirection(cardRef, monitor);
-          droplineref.current.style.top = top + "px";
-          droplineref.current.style.height = height + "px";
-          droplineref.current.style.left =
-            (direction === "left" ? left : left + width) + "px";
-        }
-      },
-    }),
-    [uid]
-  );
 
   return (
-    <div
-      onClick={onClick}
-      ref={mergeRefs([drag, drop, cardRef])}
-      className={clsx("preview_card__wrapper", {
-        "card_wrapper--active": isactive,
-        "card_wrapper--dragging": isDragging,
-      })}
-      style={{}}
-    >
-      {props.children}
-    </div>
+    <Draggable key={uid} draggableId={uid} index={index}>
+      {(provided, snapshot) => (
+        <div
+          onClick={onClick}
+          ref={mergeRefs([provided.innerRef, cardRef])}
+          className={clsx("preview_card__wrapper", {
+            "card_wrapper--active": isactive,
+            "card_wrapper--dragging": snapshot.isDragging,
+          })}
+          style={{...provided.draggableProps.style}}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {props.children}
+        </div>
+      )}
+    </Draggable>
   );
 };
 
-function AddBlock() {
+function AddBlock({ hide }: {
+  hide: boolean
+}) {
   const [, setFormState] = useFormState()!;
   const handleClick = () => {
     setFormState((form) => ({
@@ -184,11 +156,16 @@ function AddBlock() {
     }));
   };
 
+  const style = hide ? {
+    display: 'none'
+  }: undefined;
+
   return (
     <div
       onClick={handleClick}
       title="Add Block"
       className="add-slide-container"
+      style={style}
     >
       +
     </div>
